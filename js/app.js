c1674b3244c707451c861c5396d28f15f1ab0240
Jess Luu <jessica.luu@edu.sait.ca>
2021-02-24 09:37:28 -0700

    updated images and image information written

--- js/app.js
+++ js/app.js
@@ -1,2 +1,67 @@
 "use strict mode";
 
+const image = {
+  1: {
+    "title": "Au Secours!",
+    "summary": "A 1924 short silent comedy film directed by Abel Gance and written/starring Max Linder.",
+    "width": 1048,
+    "imgURL": "https://en.wikipedia.org/wiki/Au_Secours!",
+    "imgCredit": ""
+  },
+  2: {
+    "title": "Battleship Potemkin",
+    "summary": "A 1925 Soviet propoganda silent film directed by Sergei Eisenstein.",
+    "width": 1200,
+    "imgURL": "https://en.wikipedia.org/wiki/Battleship_Potemkin",
+    "imgCredit": "https://en.wikipedia.org/wiki/Battleship_Potemkin#/media/File:Vintage_Potemkin.jpg"
+  },
+  3: {
+    "title": "Carmen",
+    "summary": "A 1915 silent drama film directed by Raoul Walsh -- a founding member of the Motion Picture Academy.",
+    "width": 848,
+    "imgURL": "https://en.wikipedia.org/wiki/Carmen_(1915_Raoul_Walsh_film)",
+    "imgCredit": "https://upload.wikimedia.org/wikipedia/commons/0/01/Theda_Bara_Carmen_poster.jpg"
+  },
+  4: {
+    "title": "Girl Shy",
+    "summary": "Considered Harold Lloyd's departure from \"gag films\", it is a 1924 silent romantic comedy.",
+    "width": 1149,
+    "imgURL": "https://en.wikipedia.org/wiki/Girl_Shy",
+    "imgCredit": ""
+  },
+  5: {
+    "title": "Greed",
+    "summary": "The 1924 Erich von Stroheim epic starring ZaSu Pitts in its reconstructed glory running 239 minutes.",
+    "width": 1212,
+    "imgURL": "https://en.wikipedia.org/wiki/Greed_(1924_film)",
+    "imgCredit": "https://en.wikipedia.org/wiki/Greed_(1924_film)#/media/File:Greed3.jpg"
+  },
+  6: {
+    "title": "The Haunted House",
+    "summary": "A 1921 silent comedy film starring Buster Keaton as an unsuspecting bank teller caught in the midst of a heist.",
+    "width": 992,
+    "imgURL": "https://en.wikipedia.org/wiki/The_Haunted_House_(1921_film)",
+    "imgCredit": "https://en.wikipedia.org/wiki/The_Haunted_House_(1921_film)#/media/File:Haunted_house1921.jpg"
+  },
+  7: {
+    "title": "Häxan",
+    "summary": "Danish director Benjamin Christensen's 1922 silent horror essay film.",
+    "width": 297,
+    "imgURL": "https://en.wikipedia.org/wiki/H%C3%A4xan",
+    "imgCredit": "https://en.wikipedia.org/wiki/H%C3%A4xan#/media/File:Haxan_sv_poster.jpg"
+  },
+  8: {
+    "title": "He Who Gets Slapped",
+    "summary": "A 1924 silent psychological thriller based on a Russian play",
+    "width": 503,
+    "imgURL": "https://en.wikipedia.org/wiki/He_Who_Gets_Slapped",
+    "imgCredit": "https://en.wikipedia.org/wiki/He_Who_Gets_Slapped#/media/File:He_Who_Gets_Slapped.jpg"
+  },
+  9: {
+    "title": "Les Vampires",
+    "summary": "French director Louis Feuillade's magnum opus initially released as ten episodes between 1915 and 1916.",
+    "width": 717,
+    "imgURL": "https://en.wikipedia.org/wiki/Les_Vampires",
+    "imgCredit": ""
+  }
+}
